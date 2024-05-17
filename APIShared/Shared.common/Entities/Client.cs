ee269b11a31b755835a558be22bfb4c7926256c0
arbavel <fabvfabv.@gmail.com>
2024-05-16 20:26:13 -0500

    arreglo Entity Client

--- APIShared/Shared.common/Entities/Client.cs
+++ APIShared/Shared.common/Entities/Client.cs
@@ -13,7 +13,6 @@ namespace Shared.Common.Entities
         public string Address { get; set; } = null!;
         public string City { get; set; } = null!;
         public string Region { get; set; }=null!;
-        public string PostalCode { get; set; }= null!;
         public string Country { get; set; } = null!;
         public string Phone { get; set; } = null!;
         public string Email { get; set; } =null!;
